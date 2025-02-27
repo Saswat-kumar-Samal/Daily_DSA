c29fed75abbc4165f0b1a7f6a0aa9311e9cac4ba
saswat kumar samal <saswatstech7@gmail.com>
2025-02-27 23:26:27 +0530

    Arrays day-11

--- Arrays/Medium_Problem/Next_Permutation.java
+++ Arrays/Medium_Problem/Next_Permutation.java
@@ -13,9 +13,48 @@ public class Next_Permutation {
 
 
     ///  this optimal solution
-    /// time --
-    /// space --
+    /// time --  nearly  O(3n)
+    /// space -- O(n) we are changing in the given array
+    static void Reverse(int[] nums , int start , int end){
+        while(start <= end){
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
+    public void nextPermutation(int[] nums) {
+        int n = nums.length;
+        // step - 1 --> find the longest prefix , means the break point
+        int idx = -1;
+        for(int i=n-2; i>=0; i--){
+            if(nums[i] < nums[i+1]){
+                // index i is our break point
+                idx = i;
+                break;
+            }
+        }
+        if(idx == -1){
+            Reverse(nums , 0 , n-1);
+            return;
+        }
 
+        // step - 2 find the number which is just grater than break point
+
+        for(int i = n-1; i>=0; i--){
+            if(nums[i] > nums[idx]){
+                int temp = nums[i];
+                nums[i] = nums[idx];
+                nums[idx] = temp;
+                break;
+            }
+        }
+
+        // now reverse the right half , bez after swapping we do not need to sort it , it is needed to be
+        // reverse
+        Reverse(nums , idx+1 , n-1);
+    }
     public static void main(String[] args) {
 
     }
