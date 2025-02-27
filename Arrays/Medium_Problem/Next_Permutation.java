f5c8dbb94938b94a4fbb0d4e8121ed2a90bc879a
saswat kumar samal <saswatstech7@gmail.com>
2025-02-27 21:52:27 +0530

    Arrays day-10

---
+++ Arrays/Medium_Problem/Next_Permutation.java
@@ -0,0 +1,22 @@
+package Daily_DSA.Arrays.Medium_Problem;
+
+public class Next_Permutation {
+
+    ///  the brute force approach is very high time complexity so we are not going to implement it , for intitution just see the notes
+    /// time -- nearly O(n! * n)
+    /// space -- O(1)
+    ///
+    ///
+
+
+
+
+
+    ///  this optimal solution
+    /// time --
+    /// space --
+
+    public static void main(String[] args) {
+
+    }
+}
